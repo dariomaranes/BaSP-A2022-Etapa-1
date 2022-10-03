ef0258fa4685d79d037f3d7e00398a905503c39a
mara666 <dario.sjs@gmail.com>
2022-10-03 11:47:33 -0300

    BaSP-W07-2022: remove modal classlist when user click in span or modal outside

--- Semana-07/javascript/login.js
+++ Semana-07/javascript/login.js
@@ -170,12 +170,14 @@ window.onload = function(){
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
         modal.style.display = "none";
+        modalMsg.classList.remove("back-green", "back-red");
     }
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
+            modalMsg.classList.remove("back-green", "back-red");
         }
     }
 }
